6153a1deb443f486b9ab422861c92cc802d8044b
Jonas <jonas.luft@hcu-hamburg.de>
2019-08-19 09:47:47 +0200

    pysplit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TraPDoOr
+## TransparenzPortal Download and Order
+
+Downloaded Baugenehmigungen from the Hamburg [Transparenzportal](http://suche.transparenz.hamburg.de/?extras_registerobject_type=baugenehm)
+Splits entries with multiple Flurstücke or Gemarkungen
---
+++ main.py
@@ -0,0 +1,33 @@
+import csv
+
+result = []
+headerline = ""
+with open('data/baugenehmigungen.csv','r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='"')
+    for row in spamreader:
+        if(len(row) < 8):
+            continue
+        
+        fssplit = row[7].split("|")
+
+        fssplit = [i.lstrip('0') for i in fssplit]
+        fssplit = list(set(fssplit))
+
+        for fs in fssplit: # flurstuecke
+            newrow = row.copy()
+            newrow[7] = fs[:]
+
+            gmsplitl = row[8].split("|")
+            gmsplit = list(set(gmsplitl))
+            for gm in gmsplit: # gemarkungen
+                newnewrow = newrow.copy()
+                newnewrow[8] = gm[:]
+
+                result.append(newnewrow)
+
+with open('data/baugenehmigungen_split.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=';',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+
+    for row in result:
+        spamwriter.writerow(row)
