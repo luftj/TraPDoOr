0213a516a849dea84cc1488882e6bff890f576de
Jonas <jonas.luft@hcu-hamburg.de>
2019-08-19 09:49:53 +0200

    comments

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ with open('data/baugenehmigungen.csv','r') as csvfile:
         
         fssplit = row[7].split("|")
 
-        fssplit = [i.lstrip('0') for i in fssplit]
-        fssplit = list(set(fssplit))
+        fssplit = [i.lstrip('0') for i in fssplit] # strip leading zeroes
+        fssplit = list(set(fssplit)) # remove duplicates
 
         for fs in fssplit: # flurstuecke
             newrow = row.copy()
             newrow[7] = fs[:]
 
             gmsplitl = row[8].split("|")
-            gmsplit = list(set(gmsplitl))
+            gmsplit = list(set(gmsplitl))   # remove duplicates
             for gm in gmsplit: # gemarkungen
                 newnewrow = newrow.copy()
                 newnewrow[8] = gm[:]
